e4476398efeae5d18097ee0729b32576bf4dbad3
macbook <p.bartnik@iceo.co>
2017-09-10 16:29:40 +0200

    Initial Commit

---
+++ VoiceCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceCalculator
+//
+//  Created by Przemysław Bartnik on 10.09.2017.
+//  Copyright © 2017 Przemysław Bartnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
